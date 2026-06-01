c52f848728ee7b0bd7fd6ad5bdb0784b164a0327
ayxan <ayxan.20145@gmail.com>
2026-06-01 23:05:48 +0400

    added cursor movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    cursor::MoveTo,
+    event::{self, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use std::io::{self, stdout};
+
+fn main() -> io::Result<()> {
+    let mut x: u16 = 0;
+    let mut y: u16 = 0;
+
+    loop {
+        enable_raw_mode()?;
+
+        execute!(stdout(), MoveTo(x, y))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('h') => {
+                    if x == 0 {
+                        continue;
+                    } else {
+                        x -= 1;
+                    }
+                }
+
+                KeyCode::Char('k') => {
+                    if y == 0 {
+                        continue;
+                    } else {
+                        y -= 1;
+                    }
+                }
+
+                KeyCode::Char('j') => y += 1,
+
+                KeyCode::Char('l') => x += 1,
+
+                KeyCode::Char('q') => break,
+
+                _ => {}
+            }
+        }
+
+        disable_raw_mode()?;
+    }
+
+    Ok(())
 }
