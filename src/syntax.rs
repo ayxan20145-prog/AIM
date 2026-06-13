c35786de14dae4971b44c817af778315f9dd1496
ayxan <ayxan.20145@gmail.com>
2026-06-13 10:27:27 +0400

    added color strings to syntax

--- src/syntax.rs
+++ src/syntax.rs
@@ -8,6 +8,7 @@ struct KeywordGroup {
 
 const RESET: &str = "\x1b[0m";
 
+const COLOR_STRING: &str = "\x1b[38;2;255;220;120m"; // yellow
 const COLOR_KEYWORD: &str = "\x1b[38;2;220;120;80m"; // orange
 const COLOR_TYPE: &str = "\x1b[38;2;100;180;240m"; // light blue
 const COLOR_CONSTANT: &str = "\x1b[38;2;240;180;60m"; // gold
@@ -15,6 +16,10 @@ const COLOR_CONTROL: &str = "\x1b[38;2;200;120;200m"; // purple
 
 static GROUPS: Lazy<Vec<KeywordGroup>> = Lazy::new(|| {
     vec![
+        KeywordGroup {
+            pattern: Regex::new(r#""([^"\\]|\\.)*""#).unwrap(),
+            color: COLOR_STRING,
+        },
         KeywordGroup {
             pattern: Regex::new(
                 r"\b(let|fn|impl|trait|struct|enum|use|mod|pub|unsafe|async|await)\b",
